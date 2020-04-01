95ace89ffe61af197d909c9cdaa0799ebb93b3b8
ekalinichev <egork92@gmail.com>
2020-04-01 11:29:19 +0200

    Update eslint configuration to ignore some rules, can disscuss later if we want to enable/fix these

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,9 +6,18 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
     "plugin:prettier/recommended"
   ],
   rules: {
-    "prettier/prettier": "error"
+    "prettier/prettier": "error",
+    "react/no-unescaped-entities": "off",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+    ]
   }
 };
