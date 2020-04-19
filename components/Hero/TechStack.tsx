501eb558fba03005b204af845dd8444390c7c123
Dmitriy Yakovlev <yakovlev.dragomir@gmail.com>
2020-04-19 19:31:25 +0200

    Replace icons with new ones

--- components/Hero/TechStack.tsx
+++ components/Hero/TechStack.tsx
@@ -12,19 +12,7 @@ const TechStackItem: FC<{
     <div className="flex">
       <div className="flex-shrink-0">
         <div className="flex items-center justify-center h-10 w-10 md:h-12 md:w-12 rounded-lg bg-blue-600">
-          <svg
-            className="h-6 w-6"
-            stroke="currentColor"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
-            />
-          </svg>
+          <img className="h-6 w-6 md:w-auto md:h-auto" src={logoSrc} />
         </div>
       </div>
       <div className="ml-4">
